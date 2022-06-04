314614ef46d9fe91601d4d0262cc92f02e4aee4c
Michael Mott <michael@mott.digital>
2022-06-04 17:27:30 -0400

    docker file added and changed port

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
+const dns = require('node:dns')
+const shortid = require('shortid')
+const url = require('url')
 const app = express();
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = 3000;
+
+
+let shortIds = []
+
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: "false" }));
+//app.use(bodyParser.json());
+
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +30,43 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+app.post('/api/shorturl', (req, res) => {
+
+  console.log('*** ids: ', shortIds)
+  longURL = req.body.url
+
+  urlParsed = url.parse(longURL)
+  console.log('*** long url:', longURL)
+  console.log('*** host: ', urlParsed.host)
+
+  // make sure we have a valid url
+  dns.lookup(urlParsed.host, (err, address, family) => {
+    if (err) {
+      res.json({error: 'invalid url' })
+    } else {
+      shortId = shortid.generate()
+      shortIds.push({shortId: shortId, longURL: longURL})
+      res.json({original_url: req.body.url, short_url: shortId })
+    }
+     
+  })
+
+
+})
+
+
+app.get('/api/shorturl/:shortid', (req, res) => {
+
+  var foundItem = shortIds.find((item, index) => {
+    if( item.shortId == req.params.shortid) {
+      return true
+    }
+  })
+  res.redirect(foundItem.longURL)
+
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
